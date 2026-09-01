c86b6d60e0e143b69ff799c101ac53ad22dfabb7
Antoni_Iwanowski <75222159+AntekIwa@users.noreply.github.com>
2026-09-01 21:52:13 +0200

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,int>> v;
+        for(int i = 0; i < position.size(); i++){
+            v.push_back({position[i], speed[i]});
+        }
+        sort(v.rbegin(), v.rend());
+        vector<double> stack;
+        for(auto p : v){
+            stack.push_back((double)(target - p.first)/ p.second);
+            if(stack.size() >= 2 && stack.back() <= stack[stack.size() - 2]) stack.pop_back();
+        }
+        return stack.size();
+    }
+};
